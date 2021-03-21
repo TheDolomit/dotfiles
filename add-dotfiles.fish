1b1a934b892e1d887dee147221775d9be97b4cf7
TheDolomit <Vhyrro>
2021-03-21 10:29:11 +0100

    Exculded the syncthing directory

--- add-dotfiles.fish
+++ add-dotfiles.fish
@@ -6,6 +6,6 @@ for file in ~/.config/*
 	set file (string split "/" $file)[-1]
 	echo Checking $file...
 
-	[ "$file" != "Caprine" ] && [ "$file" != "Bitwarden" ] && [ "$file" != "Microsoft" ] && [ "$file" != "discord" ] && [ "$file" != "BetterDiscord" ] && [ "$file" != "VirtualBox" ] && [ "$file" != "unity3d" ] && [ "$file" != "Atom" ]; and git add $fullpath; and echo Added file $file 
+	[ "$file" != "Caprine" ] && [ "$file" != "Bitwarden" ] && [ "$file" != "Microsoft" ] && [ "$file" != "discord" ] && [ "$file" != "BetterDiscord" ] && [ "$file" != "VirtualBox" ] && [ "$file" != "unity3d" ] && [ "$file" != "Atom" ] && [ "$file" != "syncthing" ]; and git add $fullpath; and echo Added file $file 
 
 end
