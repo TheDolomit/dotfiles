1794258d083e9bdba6c37718c3b5cfe5bfe50534
TheDolomit <Vhyrro>
2021-03-22 18:14:12 +0100

    Removed obs studio

--- add-dotfiles.fish
+++ add-dotfiles.fish
@@ -6,6 +6,6 @@ for file in ~/.config/*
 	set file (string split "/" $file)[-1]
 	echo Checking $file...
 
-	[ "$file" != "Caprine" ] && [ "$file" != "Bitwarden" ] && [ "$file" != "Microsoft" ] && [ "$file" != "discord" ] && [ "$file" != "BetterDiscord" ] && [ "$file" != "VirtualBox" ] && [ "$file" != "unity3d" ] && [ "$file" != "Atom" ] && [ "$file" != "syncthing" ]; and git add $fullpath; and echo Added file $file 
+	[ "$file" != "obs-studio" ] && [ "$file" != "Caprine" ] && [ "$file" != "Bitwarden" ] && [ "$file" != "Microsoft" ] && [ "$file" != "discord" ] && [ "$file" != "BetterDiscord" ] && [ "$file" != "VirtualBox" ] && [ "$file" != "unity3d" ] && [ "$file" != "Atom" ] && [ "$file" != "syncthing" ]; and git add $fullpath; and echo Added file $file 
 
 end
